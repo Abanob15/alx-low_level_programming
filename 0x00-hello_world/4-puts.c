90a8c39d5efa688dfc0da87473b61855b4da1949
Abanob <abanobm94@gmail.com>
2023-08-10 19:20:24 -0700

    Berserker

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main	Entry
+ *
+ * Description:	C program print by put function
+ *
+ * Return:	0 (success)
+*/
+
+int main(void)
+{
+	pust("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
