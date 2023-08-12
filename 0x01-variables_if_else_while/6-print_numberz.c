c9eafc06c02f8d207ccfc3d5bee20411b34ada88
Abanob <abanobm94@gmail.com>
2023-08-11 20:08:17 -0700

    BWA EA113

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description:print all single digit numbers by using putchar and int variable
+ *
+ * Return:0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*convert digit to ASCII representaion*/
+		putchar(digit + '0');
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
