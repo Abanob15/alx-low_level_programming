ba60220224077096c058ad7229ee0c75a50f96ca
Abanob <abanobm94@gmail.com>
2023-08-15 20:04:38 -0700

    RB26

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,15 @@ void jack_bauer(void)
 {
 	int min, hr;
 
-	for (min - 0; hr <= 23; hr++)
+	for (min = 0; hr <= 23; hr++)
 	{
 		for  (min = 0; min <= 59; min++)
 		{
 			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
 			_putchar(':');
-			_putcahr((min / 10) + 48);
-			_putcahr((min % 10) + 48);
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 		}
 	}
