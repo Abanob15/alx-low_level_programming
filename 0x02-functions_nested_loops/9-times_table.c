255e19b5b3907191c19c2bbffac5fbb243775b26
Abanob <abanobm94@gmail.com>
2023-08-15 21:34:32 -0700

    BWA EA113

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include"main.h"
+
+/**
+ * time_table - a function that prints the 9 times table, starting with 0
+ *
+ * Example table
+ * 0,  0,  0,  0, 0, ..
+ * 0,  1,  2,  3, 4, ..
+ *
+*/
+
+void time_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+		for  (mult = 1; mlut <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			_prod = num * mult;
+
+			/*
+			 * put space if product is a single number
+			 * place the frist digit if its two numbers
+			*/
+			if (prod <= 9)
+				_putchar(' ')
+			else
+				_putchar((prod / 10) + 48); /*get the frist digit*/
+
+			_putchar((prod % 10) + 48); /*get the second digit*/
+		}
+		_putchar('\n');
+	}
+}
