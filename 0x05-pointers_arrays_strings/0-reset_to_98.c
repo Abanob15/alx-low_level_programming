41e4e7a321d7438e09d5a248becf07e99511add0
Abanob <abanobm94@gmail.com>
2023-08-21 14:59:40 -0700

    BWA EA113

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * void reset_to_98 - takes a pointer to an int parameter
  * and updates the value it points to 98
  *
- * @n: pointer to the variable to update
+ * @n: int parameter
  *
  * Return: void
 */
