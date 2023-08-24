3bb7ffc27b943dd402498d1c0907495db06be23a
Abanob <abanobm94@gmail.com>
2023-08-24 14:29:23 -0700

    B58 TU

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *_strcat - a function that concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: returns pointer to string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	_strcat(dest, src);
+	/* _strcat(dest, '\0'); */
+
+	return (0);
+}
