ba1faace36c19c8bbb974651189af9ba1d376474
Abanob <abanobm94@gmail.com>
2023-08-24 17:02:33 -0700

    B58 TU

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strcat - a function that concatenates two strings
+ *
+ * @dest: destination
+ * @src: source
+ * @n: most number of bytes from @src
+ *
+ * Return: returns pointer to string dest
+*/
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int c, i;
+	c = 0;
+
+	/*find the size of the dest array*/
+	while (dest[c])
+		c++;
+
+	/**
+	 * src dose not need to be null termainated
+	 * if it contains n or more bytes
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c+ i] = '\0';
+
+	return (dest);
+}
