047191c359f49f504518914df9e7434821b76679
Abanob <abanobm94@gmail.com>
2023-08-27 22:46:02 -0700

    BWA EA113

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _memcpy(): function copies n bytes from memory area src to memory area dest
+ *
+ * @:
+ *
+ * Returns: to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
