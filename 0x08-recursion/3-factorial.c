8ea6b5f7f9e33ea611dfa30a42f890ff4994ea21
Abanob <abanobm94@gmail.com>
2023-08-30 17:27:49 -0700

    B58 TU

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial - find the factorial of an integer
+ *
+ * @n: an integer
+ *
+ * Return: int
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
