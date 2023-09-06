7ed35a736bc5a1b9c5ce85d7d9de16ee6c2b772a
Abanob <abanobm94@gmail.com>
2023-09-05 19:52:16 -0700

    BWA EA113

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,22 +4,16 @@
 /**
  * free_grid - frees a 2 dimensional grid
  *
- * @grid: grid to free
+ * @grid: the grid
  *
  * @height: height of the grid
- *
- * Return: void (free)
 */
 
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL || height == 0)
-		return;
-
 	for (i = 0; i < height; i++)
 		free(grid[i]);
-
 	free(grid);
 }
