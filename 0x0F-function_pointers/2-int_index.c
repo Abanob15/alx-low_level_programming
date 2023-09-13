49019d0dfaec36ad2a738e84acd201112910ceb4
Abanob <abanobm94@gmail.com>
2023-09-13 16:48:24 -0700

    RB26

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,27 @@
-i
+#include "function_pointers.h"
+
+/**
+ * int_index - indexes searches for integar
+ *
+ * @array: array of int
+ *
+ * @size: size of array
+ *
+ * @cmp: function to compare
+ *
+ * Return: int value
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (x = 0; x < size; x++)
+	{
+		if (cmp(array[x]))
+			return (x);
+	}
+	return (-1);
+}
