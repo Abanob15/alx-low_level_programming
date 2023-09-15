8ff36dd446677748dc50a3fef31df3cfb6046a98
Abanob <abanobm94@gmail.com>
2023-09-14 18:14:04 -0700

    RB26

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,1 +1,53 @@
-i
+#include "variadic_functions.h"
+
+/**
+ * print_all - Prints all of the arguments when specified
+ *
+ * @format: specifies necessary operations
+ *
+ * Return: void
+*/
+
+void print_all(const char * const format, ...)
+{
+	int i;
+	int x;
+	char *str;
+	va_list pal;
+
+	va_start(pal, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(pal, int));
+				x = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(pal, int));
+				x = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(pal, double));
+				x = 0;
+				break;
+			case 's':
+				str = va_arg(pal, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				x = 0;
+				break;
+			default:
+				x = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && x == 0)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(pal);
+}
