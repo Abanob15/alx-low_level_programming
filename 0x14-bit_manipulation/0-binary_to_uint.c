1dd8ee7ddacd1340b88f7aea74cffd3d0b7fb428
Abanob <abanobm94@gmail.com>
2023-09-28 16:59:58 -0700

    BWA EA113

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -5,7 +6,7 @@
  *
  * @b: pointing to a string of (0) and (1) chars
  *
- * Return: decimal format of the binary
+ * Return: converted number or (0)
 */
 
 unsigned int binary_to_uint(const char *b)
